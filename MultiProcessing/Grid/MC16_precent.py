b1402a42e0eeb36e11b9292e065d470aa983ea70
Thomas Nommensen <tnom6927@localhost.localdomain>
2022-11-16 14:17:43 +0100

    New Config - Pileup config.

--- MultiProcessing/Grid/MC16_precent.py
+++ MultiProcessing/Grid/MC16_precent.py
@@ -1,6 +1,6 @@
 import TopExamples.grid
 
-TopExamples.grid.Add("ttH_mc16d").datasets = [
+TopExamples.grid.Add("ttH_mc16a").datasets = [
   "mc16_13TeV.312446.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m1000.deriv.DAOD_TOPQ1.e7743_a875_r9364_p4031", 
   "mc16_13TeV.312444.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m800.deriv.DAOD_TOPQ1.e7743_a875_r9364_p4031", 
   "mc16_13TeV.312441.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m500.deriv.DAOD_TOPQ1.e7743_a875_r9364_p4031", 
@@ -10,7 +10,7 @@ TopExamples.grid.Add("ttH_mc16d").datasets = [
   "mc16_13TeV.312440.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m400.deriv.DAOD_TOPQ1.e7743_a875_r9364_p4031", 
 ]
 
-TopExamples.grid.Add("ttH_mc16a").datasets = [
+TopExamples.grid.Add("ttH_mc16d").datasets = [
   "mc16_13TeV.312446.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m1000.deriv.DAOD_TOPQ1.e7743_a875_r10201_p4031", 
   "mc16_13TeV.312440.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m400.deriv.DAOD_TOPQ1.e7743_a875_r10201_p4031", 
   "mc16_13TeV.312443.MadGraphPythia8EvtGen_noallhad_ttH_tttt_m700.deriv.DAOD_TOPQ1.e7743_a875_r10201_p4031", 
